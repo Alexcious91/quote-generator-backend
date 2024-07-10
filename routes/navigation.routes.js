c2f1001050537691a13f6a0dbcf39d163e681cf6
Alexcious91 <radiopanetiisetso@gmail.com>
2024-07-10 11:38:50 +0200

    Implemented bcrypt

--- routes/navigation.routes.js
+++ routes/navigation.routes.js
@@ -28,12 +28,13 @@ router.post("/user/register", async (req, res) => {
 
       const userCollection = collection(database, "users");
 
+      const hashedPassword = await bcrypt.hash(password, 10)
 
       await addDoc(userCollection, {
          uid: user.uid,
          username: username,
          email: email,
-         password: password,
+         password: hashedPassword,
          createdAt: new Date()
       })
       await updateProfile(user, { displayName: username })
@@ -51,28 +52,13 @@ router.post("/user/login", async (req, res) => {
    try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password)
       const user = userCredential.user
-      res.status(200).json({ message: "User logged in successfully", uid: user.uid })
+      const idToken = await user.getIdToken();
+
+      console.log("logged in succesfully", user.displayName)
+      res.status(200).json({ message: "User logged in successfully: ", idToken })
    } catch (error) {
       console.error(`Error signing user in: ${error}`)
       res.status(401).send("Unauthorized")
-   }
-})
-
-router.post("/new/quote", async (req, res) => {
-   const { author, quote } = req.body;
-   const { userEmail, userSnapshot } = getUserDetails()
-
-   try {
-      const quotesCollection = collection(database, "quotes");
-      await addDoc(quotesCollection, {
-         author: userEmail,
-         quote: quote,
-         createdAt: new Date()
-      })
-      res.status(201).send("Quote added successfully")
-   } catch (error) {
-      console.error(`Error adding qoute: ${error}`)
-      res.status(500).send("Internal Server Error")
    }
 })
 
@@ -96,33 +82,46 @@ router.post("/user/edit-profile", async (req, res) => {
 
 router.get("/user/details", async (req, res) => {
    try {
-      const user = auth.currentUser;
-      if (user) {
-         res.status(501).send("Unauthorized: no user logged in")
-      }
-
-      const userDocRef = doc(database, "users", auth.currentUser.uid)
-      const userSnapshot = await getDoc(userDocRef)
-
-      if (userSnapshot.exists()) {
-         const userData = userSnapshot.data();
-         res.status(200).json(userData)
-      } else {
-         res.status("404").send("User not found")
-      }
+      auth.onAuthStateChanged(async (user) => {
+         if (user) {
+            console.log(`User details: ${user}`)
+            return res.status(200).json(user)
+         } else {
+            return res.status(401).json({ message: "Unauthorized: no user logged in" })
+         }
+      })
    } catch (error) {
+      console.error(`[ERROR]: ${error.message}`)
       res.status(500).send("Internal Server Error")
    }
-})
+});
+
 
 router.get("/user/logout", async (req, res) => {
    try {
-      const response = await signOut(auth)
+      await signOut(auth)
 
-      console.log(response)
       res.status(200).send("Successfully logged out")
    } catch (error) {
       res.status(500).send("Couldn't sign user out, try again.")
+   }
+})
+
+router.post("/new/quote", async (req, res) => {
+   const { author, quote } = req.body;
+   const { userEmail, userSnapshot } = getUserDetails()
+
+   try {
+      const quotesCollection = collection(database, "quotes");
+      await addDoc(quotesCollection, {
+         author: userEmail,
+         quote: quote,
+         createdAt: new Date()
+      })
+      res.status(201).send("Quote added successfully")
+   } catch (error) {
+      console.error(`Error adding qoute: ${error}`)
+      res.status(500).send("Internal Server Error")
    }
 })
 
