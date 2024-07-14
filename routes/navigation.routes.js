65ff9f7e9482d70933bbe1dd6a8be8da52a43e7b
Alexcious91 <radiopanetiisetso@gmail.com>
2024-07-15 01:15:16 +0200

    Refactored error handling

--- routes/navigation.routes.js
+++ routes/navigation.routes.js
@@ -113,7 +113,7 @@ router.get("/user/details", async (req, res) => {
          if (user) {
             return res.status(200).json(user)
          } else {
-            return res.status(500).json({ error: "Unauthorized: no user logged in" })
+            res.status(500).json({ error: "Unauthorized: no user logged in" })
          }
       })
    } catch (error) {
